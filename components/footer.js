e0aa438f3b659c334930b8e3628c730cb573d107
dasveloper <28981975+dasveloper@users.noreply.github.com>
2023-11-05 11:09:30 -0500

    Update copytright

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="mt-12 border-t pt-8">
-      <p className="text-base xl:text-center">&copy; 2022 Timer Page, Inc. All rights reserved.</p>
+      <p className="text-base xl:text-center">&copy; 2023 Timer Page, Inc. All rights reserved.</p>
     </div>
   )
 }
